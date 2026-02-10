97db44ecaef4dcea5794e08c052e9a0874f2ca07
Anneke1 <119932388+Anneke1@users.noreply.github.com>
2026-02-10 16:24:02 +0100

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,51 @@
+import { useState } from "react";
 import "../style.css";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <a className="navbar-logo" href="">
+      <a className="navbar-logo" href="#">
         Portfolio Anneke Clausen
       </a>
-      <ul className="navbar-links">
+
+      {/* Hamburger Icon */}
+      <div
+        className={`hamburger ${isOpen ? "open" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menü-Links */}
+      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
         <li>
-          <a href="#about">About</a>
+          <a href="#about" onClick={() => setIsOpen(false)}>
+            About
+          </a>
         </li>
         <li>
-          <a href="#experience">Experience</a>
+          <a href="#experience" onClick={() => setIsOpen(false)}>
+            Experience
+          </a>
         </li>
         <li>
-          <a href="#education">Education</a>
+          <a href="#education" onClick={() => setIsOpen(false)}>
+            Education
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" onClick={() => setIsOpen(false)}>
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" onClick={() => setIsOpen(false)}>
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
