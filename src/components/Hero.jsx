d8bb5663d1a6365983eeb7a338e5ddfa9dc2e5ce
Anneke1 <119932388+Anneke1@users.noreply.github.com>
2026-02-16 11:12:57 +0100

    neuesBild

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import "../index.css";
-import profilBild from "../assets/Anneke.png";
+import profilBild from "../assets/ProfessionalAnneke.png";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 
